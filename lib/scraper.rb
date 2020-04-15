ec1e2221c1df6619d5acaa2c15b61ed7b7e05460
github username <github email address>
2020-04-15 18:21:48 +0100

    characters links are in arrays, chapter summary needs some more work

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ wiki = Nokogiri::HTML(fma_wiki)
 array = [] 
 state_alchemsit = {} 
 counter = 0 
-while counter <= 13 
+while counter <= 8
     info_array = wiki.css("table tr")[counter +1].text.strip.split("\n")
         state_alchemist = { 
         name: info_array[0],
@@ -22,20 +22,64 @@ array
 end 
 
 
-def self.links
+def self.all_links
 #gets all the links for the character & the chapter they first appear in
 fma_wiki = open("https://fma.fandom.com/wiki/Alchemist")
 wiki = Nokogiri::HTML(fma_wiki)
 array = [] 
+original_array = [] 
 base_link = "https://fma.fandom.com/"
 wiki.css("table tr a").each do|links| 
 array << base_link+links.attr("href")
 end 
 array
+array.each_with_index do |info,index| 
+if index <18
+    original_array << info
+end
+end
+original_array
 end 
 
+#for each chapter link, get the volume and series info back for the user 
+#for each character, some info from each link to return back to the user 
+
+def self.character_links
+    personal_links = [] 
+    all_links.each_with_index do |a,index|
+    if index == 0 || index.even? 
+    personal_links << a
+    end
+    end
+personal_links
+binding.pry
+end
 
 
+def self.chapter_links 
+    chapter_links = [] 
+    all_links.each_with_index do |a,index| 
+        if index.odd? 
+            chapter_links << a 
+        end
+    end
+chapter_links
+end
+
+def self.chapter_info 
+info = {}
+array = [] 
+chapter_links.each do |a|
+fma_wiki = open(a)
+wiki = Nokogiri::HTML(fma_wiki)
+info[:summary] = wiki.css("#mw-content-text p").text
+array << info 
+binding.pry
+
+end
+array 
+end
 
 
 end
+#class end - do not touch.
