f05b1f2a8c6a098c6578309375616fa12744919f
github username <github email address>
2020-04-15 14:38:58 +0100

    array of all links for the characters when they appear in the series

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,33 +10,32 @@ counter = 0
 while counter <= 13 
     info_array = wiki.css("table tr")[counter +1].text.strip.split("\n")
         state_alchemist = { 
-        name: info_array[0],  
+        name: info_array[0],
         title: info_array[1],
         rank: info_array[2],
-        first_apperance:info_array[-1]
+        first_apperance: info_array[-1],
     }
     array << state_alchemist
     counter +=1
-    # wiki.css("table tr").each_with_index do|state_alchemist, index| 
-    # info_array = wiki.css("table tr")[index+1].text.strip.split("\n")
-    # state_alchemist = { 
-    #     name: info_array[0],  
-    #     title: info_array[1],
-    #     rank: info_array[2],
-    #     first_apperance:info_array[-1]
-    # }
-    # array << state_alchemist
 end
 array
 end 
 
-def self.laws 
-#gives us the laws of state_alchemsits!
-end
 
-def self.definition
-    #definition of alchemist provided
-end
+def self.links
+#gets all the links for the character & the chapter they first appear in
+fma_wiki = open("https://fma.fandom.com/wiki/Alchemist")
+wiki = Nokogiri::HTML(fma_wiki)
+array = [] 
+base_link = "https://fma.fandom.com/"
+wiki.css("table tr a").each do|links| 
+array << base_link+links.attr("href")
+end 
+array
+end 
+
+
+
 
 
 end
