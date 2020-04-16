5ba5cde138464980c2f88adf654930d52a5a27c9
github username <github email address>
2020-04-16 22:42:44 +0100

    mass create alchemists from the Webscraper.state_alchemsit method

--- lib/alchemists.rb
+++ lib/alchemists.rb
@@ -12,12 +12,14 @@ def self.mass_create_alchemists(array_of_hashes)
     end 
 end
 
-attr_accessor :name, :rank, :title, :chapter_link, :name_link
+attr_accessor :name, :rank, :title, :chapter_link, :name_link, :first_apperance
 
-def initialize(name:, rank:, title:, name_link:, chapter_link:)
+def initialize(name: nil, rank: nil , title: nil , name_link: nil, chapter_link: nil, first_apperance: nil)
     @name = name 
     @rank = rank 
     @title = title
+    @chapter_link = chapter_link
+    @name_link = name_link
     @@all << self
 end
 
